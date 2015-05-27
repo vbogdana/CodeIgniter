6b2503b85f21f96fc9c9d319d6fdadb3eb2eef43
vbogdana <bogdana.veselinovic@yahoo.com>
2015-05-27 21:37:04 +0200

    ADD NEW GROUP/ NEEDS CSS AND VALIDATION

--- application/controllers/NewGroupController.php
+++ application/controllers/NewGroupController.php
@@ -14,30 +14,42 @@ class NewGroupController extends CI_Controller {
     }
     
     public function createGroup() {
+        $this->load->model('Group_Model', 'group');
+        $this->load->model('IsMember_Model', 'ismember');
+        $this->load->model('User_Model', 'user');
         
         //$form_data = $this->input->post();
         $groupname = $this->input->post("groupName");
         $member = $this->input->post("member");
         
-        if ($member == '') {
-            echo "Unesite clana.";
+        if ($groupname == '') {
+            echo "Unesite ime grupe. \n";
             return;
-        } else {
-            $this->load->model('User_Model', 'user');
+        }
+        if ($member == '') {
+            echo "Unesite clana. \n";
+            return;
+        } else { 
             $id_member = $this->user->exist($member);
             echo $id_member;
-                if ($id_member == '-1') {
-                    echo "Ne postoji clan.";
-                    return;
-                }
+            if ($id_member == '-1') {
+                echo "Ne postoji clan. \n";
+                return;
+            } 
         }
-        
-        
-        $this->load->model('Group_Model', 'group');
-        $this->load->model('IsMember_Model', 'ismember');
-        $id_group = $this->group->createEntry($groupname, 7);
-        $this->ismember->createEntry($id_member, $id_group, '0');
+          
+        $id_group = $this->group->createEntry($groupname);
+        // provera da li postoji grupa
+        if ($id_group == '-1') {
+            echo "NEUSPESNO KREIRANO \n";
+        } else {
+            // ubaci clana
+            if ($this->ismember->createEntry($id_member, $id_group, '0') == FALSE) {
+                echo "ON je vec clan grupe \n";
+            }
+        }    
     }
+    
 }
 
 ?>
--- application/models/IsMember_Model.php
+++ application/models/IsMember_Model.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of IsMember_Model
  *
  * @author Bogdana
  */
+
 class IsMember_Model extends CI_Model {
     //put your code here
     function __construct() {
@@ -22,10 +17,44 @@ class IsMember_Model extends CI_Model {
         return $this->db->get('ismember')->result();
     }
     
+    public function isAdmin($id_User, $id_Group) {
+        //$this->db->select();
+        $this->db->from('ismember');
+        $this->db->where('id_User', $id_User);
+        $this->db->where('id_Group', $id_Group);
+        $this->db->where('is_Admin ==', '1');
+        
+        $query = $this->db->get();
+        
+        if ($query->num_rows() == 1) {
+            return TRUE;
+        } else if ($query->num_rows() > 1) {
+            return FALSE;
+        }
+    }
+    
+    public function isMember($id_User, $id_Group) {
+        //$this->db->select('*');
+        $this->db->from('ismember');
+        $this->db->where('id_User', $id_User);
+        $this->db->where('id_Group', $id_Group);
+        
+        $query = $this->db->get();
+        
+        if ($query->num_rows() == 1) {
+            return TRUE;
+        } else if ($query->num_rows() > 1) {
+            return FALSE;
+        }
+    }
+    
     public function createEntry($id_User, $id_Group, $is_Admin) {
+        if ($this->isMember($id_User, $id_Group) == TRUE) {
+            return FALSE;
+        }
+        
         $this->load->helper('date');
         
-        //srediti kad aleksa sredi bazu
         $entry = array(
             'id_User' => $id_User,
             'id_Group' => $id_Group ,
@@ -33,6 +62,8 @@ class IsMember_Model extends CI_Model {
             'joined_On' => date('Y-m-d H:i:s')
         );
 
-        $this->db->insert('ismember', $entry); 
+        $this->db->insert('ismember', $entry);
+        return TRUE;
+        
     }
 }
