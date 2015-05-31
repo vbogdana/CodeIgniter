5fe276e135c83bb8fb7093d873f2fe9eeb2db254
vbogdana <bogdana.veselinovic@yahoo.com>
2015-06-01 00:42:38 +0200

    delete all sa mastera

--- application/views/templates/page.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* autor Bogdana */
-
-$this->load->view('templates/header', array('username'=>'vbogdana'));
-$this->load->view($menu);
-$this->load->view($container);
-$this->load->view('templates/footer');
